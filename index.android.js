beca5bb1f730d145b88f80f5f3f0ccde360c65e5
lyctea <396136337@qq.com>
2016-11-27 19:18:29 +0800

    react-native-docs
    Hello World!

---
+++ index.android.js
@@ -0,0 +1,11 @@
+import React,{ Component} from 'react';
+import {AppRegistry, Text} from 'react-native';
+
+class LuoYeCong extends Component {
+    render(){
+        return (
+            <Text>Hello world!</Text>
+        );
+    }
+}
+AppRegistry.registerComponent('LuoYeCong', () => LuoYeCong);
