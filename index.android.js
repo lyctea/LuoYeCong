6b517c887b376962f87bfe0d91ea0443dc31d2bb
lyctea <396136337@qq.com>
2016-11-28 10:00:54 +0800

    Bananas

--- index.android.js
+++ index.android.js
@@ -1,20 +1,16 @@
+import React, { Component } from 'react';
+import { AppRegistry, Image } from 'react-native';
 
-/*
- ES6模块主要有两个功能：export和import
-
- export用于对外输出本模块（一个文件可以理解为一个模块）变量的接口
-
- import用于在一个模块中加载另一个含有export接口的模块。
-*/
-import React,{ Component} from 'react';
-import {AppRegistry, Text} from 'react-native';
-
-class LuoYeCong extends Component {
-    render(){
+class Bananas extends Component {
+    render() {
+        let pic = {
+            uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+        };
         return (
-            <Text>Hello world!</Text>
+            //括号的意思是括号内部为一个js变量或表达式，需要执行后取值。
+            <Image source={pic} style={{width: 193, height: 110}}/>
         );
     }
 }
-AppRegistry.registerComponent('LuoYeCong', () => LuoYeCong);
-//AppRegistry模块则是用来告知React Native哪一个组件被注册为整个应用的根容器
+
+AppRegistry.registerComponent('LuoYeCong', () => Bananas);
